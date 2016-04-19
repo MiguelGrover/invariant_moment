b9533b5cccc206dedc5db751e0bbecb26f53217e
Miguel Grover <mickygalgro@hotmail.com>
2016-04-18 20:40:34 -0500

    first,second and third moment

--- Momentos/src/Executable/PicIdentifier.java
+++ Momentos/src/Executable/PicIdentifier.java
@@ -31,11 +31,13 @@ public class PicIdentifier {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		blackWhite1 = new BufferedImage(bwimage1.getWidth(), bwimage1.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
 		
 		Graphics2D g2d = blackWhite1.createGraphics();
         g2d.drawImage(bwimage1, 0, 0, null);
         g2d.dispose();
+        //blackWhite1 = filtrarBN(blackWhite1);
         int[][] placa = convertTo2DUsingGetRGB(blackWhite1);
         ArrayList<Position> positions = new ArrayList<Position>();
         int encontre = 0;
@@ -86,7 +88,8 @@ public class PicIdentifier {
         	double val3 = c.thirdmoment(letters.get(x));
         	double val4 = c.fourthmoment(letters.get(x));
         	double val5 = c.fifthmoment(letters.get(x));
-        	char letra = ArrayToChar.converter(val3, 2);
+        	double[] arreglo = {val1,val2,val3,val4,val5};
+        	char letra = ArrayToChar.converter(arreglo);
         	System.out.println("Letra "+letra+":");
         	System.out.println(val1);
         	System.out.println(val2);
@@ -127,4 +130,48 @@ public class PicIdentifier {
 		return array;
 	}
 	
+	public static BufferedImage filtrarBN(BufferedImage imagen){
+		int alto=imagen.getHeight(),ancho=imagen.getWidth();
+		int nuevoalto=(int)(alto-(alto*0.25+(alto-alto*0.69)));
+		BufferedImage x=new BufferedImage(ancho, nuevoalto, BufferedImage.TYPE_INT_RGB);
+		for(int i=0;i<ancho;i++){
+			for(int j=0;j<nuevoalto;j++){
+				int color=imagen.getRGB(i, j+(int)(alto*0.25));
+				/*int R,G,B;
+				B=color&255;
+				G=(color>>8)&255;
+				R=(color>>16)&255;
+				double[] hsb=RGBaHSB((short)R, (short)G, (short)B);*/
+				/*if(hsb[2]<0.4||(hsb[2]>0.2&&hsb[2]<0.6&&hsb[1]<0.1)){
+					x.setRGB(i, j, 0);
+				}else{
+					x.setRGB(i, j, 0xffffffff);
+				}*/
+				x.setRGB(i, j, color);
+			}
+		}
+		return x;
+	}
+	
+	public double[] RGBaHSB(short R,short G, short B){
+		double[] HSB=new double[3];
+		short M=(short)Math.max(R, Math.max(G, B)),m=(short)Math.min(R, Math.min(G, B)),C;
+		C=(short)(M-m);
+		if(C==0){
+			HSB[0]=0;
+		}else if(M==R){
+			HSB[0]=60*(((G-B)/(C*1.0))%6);
+		}else if(M==G){
+			HSB[0]=60*(((B-R)/(C*1.0))+2);
+		}else{
+			HSB[0]=60*(((R-G)/(C*1.0))+4);
+		}
+		HSB[1]=(C==0)?0:(C*1.0)/M;
+		HSB[2]=M/255.0;
+		
+		if(HSB[0]<0){
+			HSB[0]=360+HSB[0];
+		}
+		return HSB;
+	}
 }
--- Momentos/src/calc/ArrayToChar.java
+++ Momentos/src/calc/ArrayToChar.java
@@ -1,387 +1,248 @@
 package calc;
 
+import java.util.ArrayList;
+
 public class ArrayToChar {
 	
-	static double[] A = {
+	static double[][] array = {{//a
 			0.5897938003855737,
 			0.21207246800950463,
 			0.01650527884841988,
 			7.373299396044781E-4,
 			2.565073787435789E-6
-		};
-	static double[] B = {
+		},{//b
 			0.45572915173271833,
 			0.10165396602532471,
 			0.001287715814544424,
 			1.201796318736621E-4,
 			-2.14099244069895E-8
-	};
-	static double[] C = {
+	},{//c
 			1.012285188373346,
 			0.7016754061964546,
 			0.1328963748427687,
 			0.02757524996777798,
 			-2.5229586942916797E-4
-	};
-	static double[] D = {
+	},{//d
 			0.5159085145137136,
 			0.11898394011043895,
 			0.0015770442747445566,
 			6.670432163912953E-4,
 			6.782835954985293E-7
-	};
-	static double[] E = {
+	},{//e
 			0.760757944683519,
 			0.3878755301563055,
 			0.03246377112261607,
 			0.007460490195108307,
 			-6.922121756776463E-5
-	};
-	static double[] F = {
+	},{//f
 			0.721784468017239,
 			0.3854329277538582,
 			0.07771796643110558,
 			0.015584917904780114,
 			3.0806265858688187E-4
-	};
-	static double[] G = {
+	},{//g
 			0.638714817417561,
 			0.21738381640242116,
 			0.01735293152386946,
 			0.007077403796268214,
 			5.8540815744649223E-5
-	};
-	static double[] H = {
+	},{//h
 			0.4381816741508429,
 			0.05658958604763231,
 			3.173580600640885E-4,
 			2.7899729271414883E-4,
 			7.734165853314761E-8
-	};
-	static double[] J = {
+	},{//j
 			0.7999645228194324,
 			0.501299976812089,
 			0.12453659877065174,
 			0.04197118254967111,
 			0.0026509800223937312
-	};
-	static double[] K = {
+	},{//k
 			0.389519913502157,
 			0.07328151275713277,
 			0.0014957635522484883,
 			2.3538202709244953E-4,
 			-4.2195369469589676E-8
-	};
-	static double[] L = {
+	},{//l
 			0.652976585953733,
 			0.3197492195371324,
 			0.0631508514227505,
 			0.014112261039984947,
 			2.5827827763712815E-4
-	};
-	static double[] M = {
+	},{//m
 			0.29154622593368906,
 			0.0293476279076943,
 			5.770801291764303E-4,
 			9.872796955927336E-4,
 			7.452019085163867E-7
-	};
-	static double[] N = {
+	},{//n
 			0.32323142897303064,
 			0.03712323157592042,
 			3.339603069807386E-5,
 			1.2909037803530967E-5,
 			2.619976083045029E-10
-	};
-	static double[]	P = {
+	},{//p
 			0.46764413613041356,
 			0.09472031937659095,
 			4.998043903055578E-6,
 			3.8481644117722085E-4,
 			1.602380231276527E-8
-	};
-	static double[] R = {
+	},{//r
 			0.46764413613041356,
 			0.09472031937659095,
 			4.998043903055578E-6,
 			3.8481644117722085E-4,
 			1.602380231276527E-8
-	};
-	static double[] S = {
+	},{//s
 			0.6610362400293385,
 			0.2765791800861815,
 			0.0011645632167599303,
 			0.001148098724577057,
 			1.3217955925995715E-6
-	};
-	static double[] T = {
+	},{//t
 			0.730193604876455,
 			0.433671163565459,
 			0.08272011885223002,
 			0.03338740019899691,
 			0.0017539403746400482
-	};
-	static double[] U = {
+	},{//u
 			0.4781638021575043,
 			0.08291774129131835,
 			1.8594072296721374E-4,
 			0.0011193421207643746,
 			-4.923174574397375E-7
-	};
-	static double[] V = {
+	},{//v
 			0.38513774723587374,
 			0.09734167942003674,
 			0.0047748395546508985,
 			1.4876509753849982E-4,
 			1.2426902334381559E-7
-	};
-	static double[] W = {
+	},{//w
 			0.27140841804924837,
 			0.02461467392009108,
 			1.4685941602712112E-4,
 			4.705137398376036E-4,
 			1.2360655396640328E-7
-	};
-	static double[] X = {
+	},{//x
 			0.48205337123425407,
 			0.14424526587127323,
 			6.638373295132717E-6,
 			1.846877354060865E-7,
 			2.0413555464950403E-13
-	};
-	static double[] Y = {
+	},{//y
 			0.47437982326228373,
 			0.1517398351819329,
 			0.0393033760206857,
 			0.01218744301063425,
 			2.667324561059933E-4
-	};
-	static double[] Z = {
+	},{//z
 			0.931783005713218,
 			0.662423977596128,
 			0.0033351797436955476,
 			0.0012382469089510049,
 			2.351774861727578E-6
-	};
-	static double[] num0 = {
+	},{//0
 			0.3699543820272141,
 			0.05886605643192034,
 			4.559149566980991E-4,
 			2.5842805688963677E-4,
 			8.869373927818364E-8
-	};
-	static double[] num1 = {
+	},{//1
 			0.8213022737392872,
 			0.6254650261100068,
 			0.04298990274942971,
 			0.030658565613426628,
 			0.0011107856082030803
-	};
-	static double[] num2 = {
+	},{//2
 			0.45480216806283347,
 			0.1152609566233908,
 			0.002718143755960729,
 			9.960532060204036E-4,
 			1.3575198702503224E-6
-	};
-	static double[] num3 = {
+	},{//3
 			0.7445318833494583,
 			0.332692693336354,
 			0.03043276676753057,
 			0.006818390480735209,
 			-4.4462521094138714E-5
-	};
-	static double[] num4 = {
+	},{//4
 			0.3029524766427491,
 			0.03665642740187984,
 			0.00388239438823051,
 			2.683773284684078E-4,
 			1.472593005841523E-7
-	};
-	static double[] num5 = {
+	},{//5
 			0.5681766306685699,
 			0.1760271086901923,
 			0.004786328258471162,
 			0.0019140286867247195,
 			5.135259022247281E-6
-	};
-	static double[] num6 = {
+	},{//6
 			0.4517730653623113,
 			0.09494312154991336,
 			0.007599411346313511,
 			0.0013637719590039836,
 			3.0330012472511245E-6
-	};
-	static double[] num7 = {
+	},{//7
 			0.7632636826753835,
 			0.4081039821181458,
 			0.19888637973019405,
 			0.07970470645722677,
 			0.009709704581951587
-	};
-	static double[] num8 = {
+	},{//8
 			0.43122250587447153,
 			0.08116845028458816,
 			2.600374926820102E-4,
 			5.7129270369145164E-5,
 			6.893978206787829E-9
-	};
-	static double[] num9 = {
+	},{//9
 			0.3769536818320952,
 			0.06382071579034138,
 			0.004356656492827518,
 			9.645399265143835E-4,
 			1.6737161419160398E-6
-	};
-	static double[] guion = {
+	}
+	,{
 			0.24434140514616387,
 			0.03245546419359032,
 			2.2439750748213626E-5,
 			2.007245255143777E-6,
 			-1.2928605918296192E-11
-	};
+	}};
 	
-	
-	public static char converter(double momento, int nivel){
+	static char[] letters = {'A','B','C','D','E','F','G','H','J','K','L','M','N','P','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','-'};
+	public static char converter(double[] momento){
 		char selected = ' ';
 		double record = 100;
+		double[] momentos = null;
+		ArrayList<double[]> secondmoment = new ArrayList<double[]>();
+		ArrayList<Character> secondmomentletter = new ArrayList<Character>();
+		ArrayList<double[]> thirdmoment = new ArrayList<double[]>();
+		ArrayList<Character> thirdmomentletter = new ArrayList<Character>();
+		for(int x = 0; x < array.length; x++){
+			momentos = array[x];
+			if(Math.abs((momento[0]-momentos[0])/momento[0]) < 0.20){
+				secondmoment.add(momentos);
+				secondmomentletter.add(letters[x]);
+			}
+		}
+		for(int x = 0; x < secondmoment.size(); x++){
+			momentos = secondmoment.get(x);
+			if(Math.abs((momento[1]-momentos[1])/momento[1]) < 0.50){
+				thirdmoment.add(momentos);
+				thirdmomentletter.add(secondmomentletter.get(x));
+			}
+		}
+		for(int x = 0; x < thirdmoment.size(); x++){
+			momentos = thirdmoment.get(x);
+			if(Math.abs((momento[2]-momentos[2])/momento[2]) < record){
+				System.out.println("Error: "+Math.abs((momento[2]-momentos[2])/momento[2]));
+				selected = thirdmomentletter.get(x).charValue();
+				record = Math.abs((momento[2]-momentos[2])/momento[2]);
+			}
+		}
 		
-		if(Math.abs((momento-A[nivel])/momento) < record){
-			selected = 'A';
-			record = Math.abs((momento-A[nivel])/momento);
-		}
-		if(Math.abs((momento-B[nivel])/momento) < record){
-			selected = 'B';
-			record = Math.abs((momento-B[nivel])/momento);
-		}
-		if(Math.abs((momento-C[nivel])/momento) < record){
-			selected = 'C';
-			record = Math.abs((momento-C[nivel])/momento);
-		}
-		if(Math.abs((momento-D[nivel])/momento) < record){
-			selected = 'D';
-			record = Math.abs((momento-D[nivel])/momento);
-		}
-		if(Math.abs((momento-E[nivel])/momento) < record){
-			selected = 'E';
-			record = Math.abs((momento-E[nivel])/momento);
-		}
-		if(Math.abs((momento-F[nivel])/momento) < record){
-			selected = 'F';
-			record = Math.abs((momento-F[nivel])/momento);
-		}
-		if(Math.abs((momento-G[nivel])/momento) < record){
-			selected = 'G';
-			record = Math.abs((momento-G[nivel])/momento);
-		}
-		if(Math.abs((momento-H[nivel])/momento) < record){
-			selected = 'H';
-			record = Math.abs((momento-H[nivel])/momento);
-		}
-		if(Math.abs((momento-J[nivel])/momento) < record){
-			selected = 'J';
-			record = Math.abs((momento-J[nivel])/momento);
-		}
-		if(Math.abs((momento-K[nivel])/momento) < record){
-			selected = 'K';
-			record = Math.abs((momento-K[nivel])/momento);
-		}
-		if(Math.abs((momento-L[nivel])/momento) < record){
-			selected = 'L';
-			record = Math.abs((momento-L[nivel])/momento);
-		}
-		if(Math.abs((momento-M[nivel])/momento) < record){
-			selected = 'M';
-			record = Math.abs((momento-M[nivel])/momento);
-		}
-		if(Math.abs((momento-N[nivel])/momento) < record){
-			selected = 'N';
-			record = Math.abs((momento-N[nivel])/momento);
-		}
-		if(Math.abs((momento-P[nivel])/momento) < record){
-			selected = 'P';
-			record = Math.abs((momento-P[nivel])/momento);
-		}
-		if(Math.abs((momento-R[nivel])/momento) < record){
-			selected = 'R';
-			record = Math.abs((momento-R[nivel])/momento);
-		}
-		if(Math.abs((momento-S[nivel])/momento) < record){
-			selected = 'S';
-			record = Math.abs((momento-S[nivel])/momento);
-		}
-		if(Math.abs((momento-T[nivel])/momento) < record){
-			selected = 'T';
-			record = Math.abs((momento-T[nivel])/momento);
-		}
-		if(Math.abs((momento-U[nivel])/momento) < record){
-			selected = 'U';
-			record = Math.abs((momento-U[nivel])/momento);
-		}
-		if(Math.abs((momento-V[nivel])/momento) < record){
-			selected = 'V';
-			record = Math.abs((momento-V[nivel])/momento);
-		}
-		if(Math.abs((momento-W[nivel])/momento) < record){
-			selected = 'W';
-			record = Math.abs((momento-W[nivel])/momento);
-		}
-		if(Math.abs((momento-X[nivel])/momento) < record){
-			selected = 'X';
-			record = Math.abs((momento-X[nivel])/momento);
-		}
-		if(Math.abs((momento-Y[nivel])/momento) < record){
-			selected = 'Y';
-			record = Math.abs((momento-Y[nivel])/momento);
-		}
-		if(Math.abs((momento-Z[nivel])/momento) < record){
-			selected = 'Z';
-			record = Math.abs((momento-Z[nivel])/momento);
-		}
-		if(Math.abs((momento-num0[nivel])/momento) < record){
-			selected = '0';
-			record = Math.abs((momento-num0[nivel])/momento);
-		}
-		if(Math.abs((momento-num1[nivel])/momento) < record){
-			selected = '1';
-			record = Math.abs((momento-num1[nivel])/momento);
-		}
-		if(Math.abs((momento-num2[nivel])/momento) < record){
-			selected = '2';
-			record = Math.abs((momento-num2[nivel])/momento);
-		}
-		if(Math.abs((momento-num3[nivel])/momento) < record){
-			selected = '3';
-			record = Math.abs((momento-num3[nivel])/momento);
-		}
-		if(Math.abs((momento-num4[nivel])/momento) < record){
-			selected = '4';
-			record = Math.abs((momento-num4[nivel])/momento);
-		}
-		if(Math.abs((momento-num5[nivel])/momento) < record){
-			selected = '5';
-			record = Math.abs((momento-num5[nivel])/momento);
-		}
-		if(Math.abs((momento-num6[nivel])/momento) < record){
-			selected = '6';
-			record = Math.abs((momento-num6[nivel])/momento);
-		}
-		if(Math.abs((momento-num7[nivel])/momento) < record){
-			selected = '7';
-			record = Math.abs((momento-num7[nivel])/momento);
-		}
-		if(Math.abs((momento-num8[nivel])/momento) < record){
-			selected = '8';
-			record = Math.abs((momento-num8[nivel])/momento);
-		}
-		if(Math.abs((momento-num9[nivel])/momento) < record){
-			selected = '9';
-			record = Math.abs((momento-num9[nivel])/momento);
-		}
-		if(Math.abs((momento-guion[nivel])/momento) < record){
-			selected = '-';
-			record = Math.abs((momento-guion[nivel])/momento);
-		}
 		return selected;
 	}
 }
